1b0b82df5bb4fd42f68944ff1b06ad461942910a
Daniil Miskevich <daniilmiskevich@gmail.com>
2025-02-20 01:26:06 +0300

    Add and configure checkstyle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.2"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 
-    // sonarqube
+    checkstyle
     id("org.sonarqube") version "6.0.1.5171"
 }
 
@@ -11,9 +11,13 @@ group = "com.daniilmiskevich"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+checkstyle {
+    toolVersion = "10.21.2"
 }
 
 sonar {
@@ -25,16 +29,16 @@ sonar {
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	// testImplementation("org.springframework.boot:spring-boot-starter-test")
-	// testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    // testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
