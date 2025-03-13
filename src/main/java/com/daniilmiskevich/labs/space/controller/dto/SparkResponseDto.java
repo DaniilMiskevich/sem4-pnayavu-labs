12c5387cede986f834122d98f33e8625b7b8b88f
Daniil Miskevich <daniilmiskevich@gmail.com>
2025-03-13 23:20:20 +0300

    Fix insonsistent naming in API result fields.

--- src/main/java/com/daniilmiskevich/labs/space/controller/dto/SparkResponseDto.java
+++ src/main/java/com/daniilmiskevich/labs/space/controller/dto/SparkResponseDto.java
@@ -4,8 +4,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import com.daniilmiskevich.labs.space.model.Spark;
 import com.daniilmiskevich.labs.space.model.Spectre;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record SparkResponseDto(Long id, String name, Long spaceId, Set<String> spectreNames) {
+public record SparkResponseDto(
+    Long id,
+    String name,
+    @JsonProperty("space_id") Long spaceId,
+    @JsonProperty("spectre_names") Set<String> spectreNames) {
     public SparkResponseDto(Spark spark) {
         this(
             spark.getId(),
